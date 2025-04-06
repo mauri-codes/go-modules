40d4b9cb67de104a223c29b584995d784d698ac5
Mauricio Alarcon <mauricio.alarcon1789@gmail.com>
2025-04-06 03:35:00 -0400

    added responses utils

---
+++ utils/responses.go
@@ -0,0 +1,48 @@
+package utils
+
+import (
+	"encoding/json"
+
+	"github.com/aws/aws-lambda-go/events"
+)
+
+type LambdaResponse struct {
+	Success bool
+	Message *string
+	Data    any
+}
+
+type ResponseInput struct {
+	Message string
+	Code    int
+	Data    any
+}
+
+func Error400(input ResponseInput) events.APIGatewayProxyResponse {
+	input.Code = 400
+	return ErrorResponse(input)
+}
+
+func Error500(input ResponseInput) events.APIGatewayProxyResponse {
+	input.Code = 500
+	return ErrorResponse(input)
+}
+
+func ErrorResponse(input ResponseInput) events.APIGatewayProxyResponse {
+	out, _ := json.Marshal(LambdaResponse{
+		Message: &input.Message,
+		Success: false,
+		Data:    input.Message,
+	})
+	return events.APIGatewayProxyResponse{Body: string(out), StatusCode: input.Code}
+}
+
+func SuccessResponse(event ResponseInput) events.APIGatewayProxyResponse {
+	var response = LambdaResponse{
+		Success: true,
+		Message: &event.Message,
+		Data:    event.Data,
+	}
+	out, _ := json.Marshal(response)
+	return events.APIGatewayProxyResponse{Body: string(out), StatusCode: 200}
+}
