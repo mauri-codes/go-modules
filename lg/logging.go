f6f1c045c1868f0050acba5c60e690a1f5dc22f4
mauri-codes <mauricio.alarcon1789@gmail.com>
2026-02-02 04:35:49 -0400

    made isAWS function private

--- lg/logging.go
+++ lg/logging.go
@@ -9,13 +9,13 @@ import (
 
 func P(data any) {
 	strLog, _ := json.MarshalIndent(data, "", "\t")
-	if IsAWS() {
+	if isAWS() {
 		strLog, _ = json.Marshal(data)
 	}
 	fmt.Println(string(strLog))
 }
 
-func IsAWS() bool {
+func isAWS() bool {
 	env := os.Getenv("AWS_EXECUTION_ENV")
 	return strings.Contains(env, "ECS") || strings.Contains(env, "EC2") || os.Getenv("ECS_CONTAINER_METADATA_URI_V4") != ""
 }
