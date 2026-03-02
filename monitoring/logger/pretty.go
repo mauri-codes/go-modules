267415f61c1298e2c7946bf6cefc298bcd85f734
mauri-codes <mauricio.alarcon1789@gmail.com>
2026-03-02 01:07:28 -0400

    added Pretty logs

--- monitoring/logger/pretty.go
+++ monitoring/logger/pretty.go
@@ -1,8 +1,11 @@
 package logger
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"log/slog"
+	"os"
 )
 
 func Pretty(v any) string {
@@ -11,4 +14,49 @@ func Pretty(v any) string {
 		return fmt.Sprintf("error marshaling: %v", err)
 	}
 	return string(b)
+}
+
+func InfoPretty(ctx context.Context, log *slog.Logger, msg string, v any) {
+	if !log.Enabled(ctx, slog.LevelInfo) {
+		return
+	}
+
+	b, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		log.Info("dump failed", "msg", msg, "err", err)
+		return
+	}
+	log.InfoContext(ctx, msg)
+	_, _ = os.Stdout.Write(b)
+	_, _ = os.Stdout.Write([]byte("\n"))
+}
+
+func DebugPretty(ctx context.Context, log *slog.Logger, msg string, v any) {
+	if !log.Enabled(ctx, slog.LevelDebug) {
+		return
+	}
+
+	b, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		log.Info("dump failed", "msg", msg, "err", err)
+		return
+	}
+	log.InfoContext(ctx, msg)
+	_, _ = os.Stdout.Write(b)
+	_, _ = os.Stdout.Write([]byte("\n"))
+}
+
+func ErrorPretty(ctx context.Context, log *slog.Logger, msg string, v any) {
+	if !log.Enabled(ctx, slog.LevelError) {
+		return
+	}
+
+	b, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		log.Info("dump failed", "msg", msg, "err", err)
+		return
+	}
+	log.InfoContext(ctx, msg)
+	_, _ = os.Stdout.Write(b)
+	_, _ = os.Stdout.Write([]byte("\n"))
 }
