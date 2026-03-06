851638b5254c7d838f4fa06d99ade2a2e30eaf99
mauri-codes <mauricio.alarcon1789@gmail.com>
2026-03-05 20:16:03 -0400

    added Get EnvVar methods

--- utils/utils.go
+++ utils/utils.go
@@ -1,5 +1,27 @@
 package utils
 
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
 func StrToPtr(s string) *string {
 	return &s
+}
+
+func GetRequiredEnvVar(envVarName string) (string, error) {
+	envVarValue := strings.TrimSpace(os.Getenv(envVarName))
+	if envVarValue == "" {
+		return "", fmt.Errorf("environment variable %s is not set", envVarName)
+	}
+	return envVarValue, nil
+}
+
+func GetOptionalEnvVar(envVarName string, defaultValue string) string {
+	envVarValue := strings.TrimSpace(os.Getenv(envVarName))
+	if envVarValue == "" {
+		return defaultValue
+	}
+	return envVarValue
 }
