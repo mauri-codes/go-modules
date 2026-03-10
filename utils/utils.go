95fe50b13611c46db1821aa1d795c4e07e747ff3
mauri-codes <mauricio.alarcon1789@gmail.com>
2026-03-10 10:24:09 -0400

    added BoolToPtr

--- utils/utils.go
+++ utils/utils.go
@@ -10,6 +10,10 @@ func StrToPtr(s string) *string {
 	return &s
 }
 
+func BoolToPtr(b bool) *bool {
+	return &b
+}
+
 func GetRequiredEnvVar(envVarName string) (string, error) {
 	envVarValue := strings.TrimSpace(os.Getenv(envVarName))
 	if envVarValue == "" {
